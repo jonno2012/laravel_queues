a07ca4a237d77c2f316669eb521fd0cb14ab0da0
jonno2012 <jonathanrhill78@gmail.com>
2023-05-04 17:54:19 +0100

    Basic job dispatched + first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    (new \App\Jobs\SendWelcomeEmail())->handle();
+
+    \App\Jobs\SendWelcomeEmail::dispatch();
+
+    return view('welcome');
+});
