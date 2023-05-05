bf835e424fdb1c187ebaa00f4d2772b0b1225af4
jonno2012 <jonathanrhill78@gmail.com>
2023-05-05 11:38:03 +0100

    more complex workflows

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', function () {
+Route::get('/workflows1', function () {
 //   $chain = [
 //       new \App\Jobs\PullRepo(),
 //       new \App\Jobs\RunTests(),
@@ -42,8 +42,54 @@ Route::get('/', function () {
 
 //   \Illuminate\Support\Facades\Bus::chain($chain)->dispatch();
     \Illuminate\Support\Facades\Bus::batch($batch)
-//        ->allowFailures()
+        ->allowFailures()
+        ->catch(function($batch, $e) {
+            // to be executed when a job fails
+        })
+        ->then(function ($batch) {
+            // to be executed if no jobs fail and once they have all completed
+        })
+        ->finally(function($batch) {
+            // will execute if jobs complete even if one of them has failed
+        })
+        ->onQueue('deployments')
+        ->onConnection('database')
         ->dispatch();
 
    return view('welcome');
+});
+
+Route::get('/workflows2', function() {
+    // we can dispatch a chain within a batch:
+    // * both batches will be executed in parallel.
+    $batch = [
+        [
+            new \App\Jobs\PullRepo(),
+            new \App\Jobs\RunTests(),
+            new \App\Jobs\Deploy()
+        ],
+        [
+            new \App\Jobs\PullRepo(),
+            new \App\Jobs\RunTests(),
+            new \App\Jobs\Deploy()
+        ],
+    ];
+
+    \Illuminate\Support\Facades\Bus::batch($batch)
+        ->allowFailures()
+        ->dispatch();
+
+    // we can also call a batch inside a chain
+    \Illuminate\Support\Facades\Bus::chain([
+        new \App\Jobs\Deploy(),
+        function() {
+        \Illuminate\Support\Facades\Bus::batch([
+            new \App\Jobs\PullRepo(),
+            new \App\Jobs\PullRepo(),
+            new \App\Jobs\PullRepo(),
+        ])->dispatch();
+        }
+    ]);
+
+    return view('welcome');
 });
